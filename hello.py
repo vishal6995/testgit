54356ef67ad17185abc5754f9a48d0e6dd82c4b2
S <vishas1@apac.corpdir.net>
2023-01-11 14:25:31 +0530

    Added new line using spyder git integration

--- hello.py
+++ hello.py
@@ -5,3 +5,5 @@ print("Learn the basic commands: clone, add, status, commit, push")
 print("Added new line")
 
 print("Commit this line to new branch")
+
+print("Added a fresh line into reviewer branch")
